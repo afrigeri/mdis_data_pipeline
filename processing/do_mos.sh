d0693bc380e57df8c3284a0aab88b658eb124dde
Alessandro Frigeri <alessandro.frigeri@gmail.com>
2018-09-16 19:19:20 +0200

    added link to the image of the mosaic

--- processing/do_mos.sh
+++ processing/do_mos.sh
@@ -1,1 +1,2 @@
 automos fromlist=mos.lis mosaic=RaditladiBasin.cub
+isis2std from=RaditladiBasin.cub  to=RaditladiBasin.png
--- processing/mdis_pipeline.sh
+++ processing/mdis_pipeline.sh
@@ -1,5 +1,7 @@
-./do_L1.sh
-./do_L2.sh
+#
+# Mosaic without photometric correction
+#./do_L1.sh
+#./do_L2.sh
 ls L2/*.cub > mos.lis
 ./do_mos.sh
 
